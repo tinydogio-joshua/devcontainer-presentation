8f23f039c03be7dffe99f5a1ab21cc24f20440a9
Joshua Hamilton <joshua@tinydog.io>
2023-01-31 18:55:39 +0000

    Basic server.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use sqlx::{postgres::PgPoolOptions, Error as SQLxError, PgPool};
+use std::net::SocketAddr;
+
+async fn db() -> Result<PgPool, SQLxError> {
+    let connection_string = "postgres://postgres:postgres@localhost:5432/postgres";
+
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(connection_string)
+        .await
+}
+
+async fn index_handler() -> Html<&'static str> {
+    Html("<h1>Hello, World</h1>")
+}
+
+async fn run_migrations(pool: &PgPool) {
+    sqlx::migrate!()
+        .run(pool)
+        .await
+        .expect("🚨 Unable to Run Migrations");
+    println!("✅ Migrations Complete");
+}
+
+async fn run_server() {
+    let app = Router::new().route("/", get(index_handler));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
+
+#[tokio::main]
+async fn main() {
+    match db().await {
+        Ok(pool) => {
+            run_migrations(&pool).await;
+            run_server().await;
+        }
+        Err(_) => println!("🚨 Unable To Connect To Database"),
+    }
 }
